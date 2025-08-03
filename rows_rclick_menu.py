6c292cec0c6c228f8d6cc89ca26e1bf743bf89fb
Oleksiy Zakharov <alexzkhr@gmail.com>
2025-08-03 19:33:51 +0300

    Made menu more declarative.

--- rows_rclick_menu.py
+++ rows_rclick_menu.py
@@ -1,6 +1,19 @@
 import tkinter as tk
+from typing import Callable
 from cargo_names import MarketName
 from _logger import logger
+
+
+class _MenuCommand:
+    def __init__(self, label: str, handler: Callable[[], None]):
+        self.label = label
+        self.handler = handler
+
+
+class _MenuSeparator:
+    """Marker class to insert separator into menu."""
+
+    pass
 
 
 class RightClickContextMenuForTable:
@@ -13,36 +26,31 @@ class RightClickContextMenuForTable:
         self._commodity = commodity
 
         self._menu = tk.Menu(self._parent, tearoff=0)
+
+        self._commands: list[_MenuCommand | _MenuSeparator] = [
+            _MenuCommand(
+                f"Copy: {self._market.trade_name}",
+                lambda: self._copy_to_clipboard(self._market.trade_name),
+            ),
+            _MenuSeparator(),
+            _MenuCommand(f"Cancel: {self._market.trade_name}", lambda: None),
+        ]
+
         self._build_menu()
-
-    def _build_menu(self):
-        self._menu.add_command(
-            label=f"Copy: {self._commodity}", command=self._copy_commodity
-        )
-        self._menu.add_command(
-            label=f"Show market: {self._market}", command=self._show_market_info
-        )
-        self._menu.add_separator()
-        self._menu.add_command(label="Debug info", command=self._log_debug_info)
-
-        self._menu.add_separator()
-        self._menu.add_command(label="Cancel", command=lambda: None)
-
-    def _copy_commodity(self):
-        self._parent.clipboard_clear()
-        self._parent.clipboard_append(self._commodity)
-
-    def _show_market_info(self):
-        logger.info(f"Market info requested: {self._market}")
-        # TODO
-
-    def _log_debug_info(self):
-        logger.debug(
-            f"Commodity: {self._commodity}, Market: {self._market}, Amount: {self._amount}"
-        )
 
     def popup(self, event: tk.Event):
         try:
             self._menu.tk_popup(event.x_root, event.y_root)
         finally:
             self._menu.grab_release()
+
+    def _build_menu(self):
+        for cmd in self._commands:
+            if isinstance(cmd, _MenuCommand):
+                self._menu.add_command(label=cmd.label, command=cmd.handler)
+            else:
+                self._menu.add_separator()
+
+    def _copy_to_clipboard(self, text: str):
+        self._parent.clipboard_clear()
+        self._parent.clipboard_append(text)
